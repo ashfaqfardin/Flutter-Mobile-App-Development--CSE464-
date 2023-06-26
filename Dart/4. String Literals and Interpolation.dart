e0dbfc6b4ff673776f5768801b639086418b77cd
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-26 23:52:56 +0600

    -- few modifications

--- Dart/4. String Literals and Interpolation.dart
+++ Dart/4. String Literals and Interpolation.dart
@@ -1,18 +1,18 @@
 void main(){
   // Literals
-  var isCool = true;
-  int x = 2;
+  // var isCool = true;
+  // int x = 2;
   "john";
   4.5;
 
   // Various ways to define Literals in Dart
-  String s1 = 'Single';
-  String s2 = "Double";
-  String s3 = 'It\'s easy'; //backslash
-  String s4 = "It's easy";
+  // String s1 = 'Single';
+  // String s2 = "Double";
+  // String s3 = 'It\'s easy'; //backslash
+  // String s4 = "It's easy";
 
-  String s5 = 'This is going to be a very long String.'
-              'This is just a simple demo';
+  // String s5 = 'This is going to be a very long String.'
+  //             'This is just a simple demo';
   
   //String Interpolation
   // we should use ["My name is $name"] instead of ["My name is " + name]
