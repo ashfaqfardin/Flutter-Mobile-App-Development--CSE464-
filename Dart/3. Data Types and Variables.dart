72a0a5a6e20a81f5fa8e2dc2e3d74559b5e5996c
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-26 20:03:32 +0600

    --Data Types

---
+++ Dart/3. Data Types and Variables.dart
@@ -0,0 +1,25 @@
+void main(){
+  //all the datatype are
+  //basiclly objects
+  //deafult value is 'null'
+
+  //numbers
+  int age = 10;
+  int hexVal = 0xFABCDEEF;
+  // var age = 10; //OR we can use this
+  double cgpa = 3.33;
+  double exponent = 1.32e5;
+
+  String name = "Ashfaq";
+  // var name = "Ashfaq";
+
+  bool isAlive = true;
+  // var isAlive = true;
+
+  print(age);
+  print(cgpa);
+  print(name);
+  print(isAlive);
+  print(hexVal);
+  print(exponent);
+}
