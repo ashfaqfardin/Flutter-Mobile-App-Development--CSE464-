d9bb623c6da8da8e2c66d7a93d437741ff44e5d1
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-27 00:18:35 +0600

    -- final vs const

---
+++ Dart/5. Constants.dart
@@ -0,0 +1,17 @@
+void main(){
+  // final keyword
+  final cityName = 'Dhaka';
+  //cityName = 'CTG'; x
+  print(cityName);
+  
+  // const keyword
+  const PI = 3.14;
+  const double gravity = 9.8;
+  print(PI);
+  print(gravity);
+}
+
+class Circle {
+  final color = 'Red';
+  static const PI = 3.14;
+}
