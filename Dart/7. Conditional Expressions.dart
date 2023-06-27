d792b32de8b2ab402cc0e014f12bbabc2ffd9254
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-28 01:58:52 +0600

    --conditional expression

--- Dart/7. Conditional Expressions.dart
+++ Dart/7. Conditional Expressions.dart
@@ -2,7 +2,7 @@ void main(){
   int a = 2;
   int b = 3;
 
-  int smallNumber;
+  //int smallNumber;
   // if(a < b){
   //   smallNumber = a;
   // } else {
@@ -13,6 +13,17 @@ void main(){
 
   // condition ? exp1: exp2;
   //a < b ? print("$a is smaller"):print("$b is smaller");
-  smallNumber = a < b ? a : b;
+  int smallNumber = a < b ? a : b;
   print("$smallNumber is smaller");
+
+  // older dart version
+  // exp1 ?? exp2
+  // if exp1 is not-null, returns its value
+  // otherwise evalutes and returns value of exp2
+  // String name = "ashfaq";
+
+  // String nameToPrint = name ?? "Guest User";
+  // print(nameToPrint);
+
+
 }
