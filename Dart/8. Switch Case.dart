d0628dbe2a693281f0f2f505f0f1f85a5f5d9812
ashfaqfardin <imashfaqfardin@gmail.com>
2023-06-29 00:37:29 +0600

    -- switch cases

---
+++ Dart/8. Switch Case.dart
@@ -0,0 +1,17 @@
+void main(){
+  String grade = 'A';
+
+  // parameter should be int or String
+  switch(grade){
+    case 'A':
+      print("Excellent");
+      break;
+    case 'B':
+      print("Moderate");
+      break;
+    default:
+      print("Fail");
+      break;
+  }
+
+}
